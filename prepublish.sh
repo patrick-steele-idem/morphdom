e9184fc50af3790258c22fd5f43b4dc1c0d5dcfe
Patrick Steele-Idem <pnidem@gmail.com>
2016-03-21 09:29:30 -0600

    Add a minified UMD distribution

---
+++ prepublish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -o errexit # Exit on error
+mkdir -p dist
+node_modules/umd/bin/cli.js morphdom lib/index.js -c > dist/morphdom-umd.js
+node_modules/.bin/uglifyjs ./dist/morphdom-umd.js -o ./dist/morphdom-umd.min.js
