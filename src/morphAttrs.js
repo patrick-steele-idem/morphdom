915b90cf48f149324b9436c2235c4390937d4861
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-04-18 20:54:59 +0200

    fixed missing semis

--- src/morphAttrs.js
+++ src/morphAttrs.js
@@ -16,7 +16,7 @@ export default function morphAttrs(fromNode, toNode) {
         attrValue = attr.value;
 
         if (attrNamespaceURI) {
-            var attrLocalName = attr.localName
+            var attrLocalName = attr.localName;
 
             // Important: getAttributeNS expects the localName of a namespaced attribute
             // but setAttributeNS requires the fully qualified name
@@ -24,9 +24,9 @@ export default function morphAttrs(fromNode, toNode) {
             // ref: https://www.w3.org/TR/DOM-Level-2-Core/glossary.html#dt-localname
             // ref: https://dom.spec.whatwg.org/#dom-element-setattributens
             // ref: https://www.w3.org/TR/DOM-Level-2-Core/glossary.html#dt-qualifiedname
-            fromValue = fromNode.getAttributeNS(attrNamespaceURI, attrLocalName)
+            fromValue = fromNode.getAttributeNS(attrNamespaceURI, attrLocalName);
             if (fromValue !== attrValue) {
-                fromNode.setAttributeNS(attrNamespaceURI, attrName, attrValue)
+                fromNode.setAttributeNS(attrNamespaceURI, attrName, attrValue);
             }
         } else {
             fromValue = fromNode.getAttribute(attrName);
