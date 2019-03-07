3bdf2c5e9a489ea86ba3a7fd0b0fc36107856e7e
Scott Newcomer <snewcomer@users.noreply.github.com>
2019-03-07 07:51:24 -0800

    Add Typescript declaration file (#138)

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+interface MorphDomOptions {
+    getNodeKey?: (node: Node) => any;
+    onBeforeNodeAdded?: (node: Node) => Node;
+    onNodeAdded?: (node: Node) => Node;
+    onBeforeElUpdated?: (fromEl: HTMLElement, toEl: HTMLElement) => boolean;
+    onElUpdated?: (el: HTMLElement) => void;
+    onBeforeNodeDiscarded?: (node: Node) => boolean;
+    onNodeDiscarded?: (node: Node) => void;
+    onBeforeElChildrenUpdated?: (fromEl: HTMLElement, toEl: HTMLElement) => boolean;
+    childrenOnly?: boolean;
+}
+
+declare function morphdom(
+    fromNode: Node,
+    toNode: Node | string,
+    options?: MorphDomOptions,
+): void;
+
+export = morphdom;
