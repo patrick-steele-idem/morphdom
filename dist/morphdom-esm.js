9c54adacda2c8eb06069dd863e3257b613c50d71
snewcomer <snewcomer24@gmail.com>
2019-03-29 16:54:10 -0700

    cleanup fromEl put in own function

--- dist/morphdom-esm.js
+++ dist/morphdom-esm.js
@@ -416,6 +416,25 @@ function morphdomFactory(morphAttrs) {
             }
         }
 
+        function cleanupFromEl(fromEl, curFromNodeChild, curFromNodeKey) {
+            // We have processed all of the "to nodes". If curFromNodeChild is
+            // non-null then we still have some from nodes left over that need
+            // to be removed
+            while (curFromNodeChild) {
+                var fromNextSibling = curFromNodeChild.nextSibling;
+                if ((curFromNodeKey = getNodeKey(curFromNodeChild))) {
+                    // Since the node is keyed it might be matched up later so we defer
+                    // the actual removal to later
+                    addKeyedRemoval(curFromNodeKey);
+                } else {
+                    // NOTE: we skip nested keyed nodes from being removed since there is
+                    //       still a chance they will be matched up later
+                    removeNode(curFromNodeChild, fromEl, true /* skip keyed nodes */);
+                }
+                curFromNodeChild = fromNextSibling;
+            }
+        }
+
         function morphEl(fromEl, toEl, childrenOnly) {
             var toElKey = getNodeKey(toEl);
 
@@ -612,22 +631,7 @@ function morphdomFactory(morphAttrs) {
                 curFromNodeChild = fromNextSibling;
             }
 
-            // We have processed all of the "to nodes". If curFromNodeChild is
-            // non-null then we still have some from nodes left over that need
-            // to be removed
-            while (curFromNodeChild) {
-                fromNextSibling = curFromNodeChild.nextSibling;
-                if ((curFromNodeKey = getNodeKey(curFromNodeChild))) {
-                    // Since the node is keyed it might be matched up later so we defer
-                    // the actual removal to later
-                    addKeyedRemoval(curFromNodeKey);
-                } else {
-                    // NOTE: we skip nested keyed nodes from being removed since there is
-                    //       still a chance they will be matched up later
-                    removeNode(curFromNodeChild, fromEl, true /* skip keyed nodes */);
-                }
-                curFromNodeChild = fromNextSibling;
-            }
+            cleanupFromEl(fromEl, curFromNodeChild, curFromNodeKey);
 
             var specialElHandler = specialElHandlers[fromEl.nodeName];
             if (specialElHandler) {
--- dist/morphdom-umd.js
+++ dist/morphdom-umd.js
@@ -422,6 +422,25 @@
                 }
             }
 
+            function cleanupFromEl(fromEl, curFromNodeChild, curFromNodeKey) {
+                // We have processed all of the "to nodes". If curFromNodeChild is
+                // non-null then we still have some from nodes left over that need
+                // to be removed
+                while (curFromNodeChild) {
+                    var fromNextSibling = curFromNodeChild.nextSibling;
+                    if ((curFromNodeKey = getNodeKey(curFromNodeChild))) {
+                        // Since the node is keyed it might be matched up later so we defer
+                        // the actual removal to later
+                        addKeyedRemoval(curFromNodeKey);
+                    } else {
+                        // NOTE: we skip nested keyed nodes from being removed since there is
+                        //       still a chance they will be matched up later
+                        removeNode(curFromNodeChild, fromEl, true /* skip keyed nodes */);
+                    }
+                    curFromNodeChild = fromNextSibling;
+                }
+            }
+
             function morphEl(fromEl, toEl, childrenOnly) {
                 var toElKey = getNodeKey(toEl);
 
@@ -618,22 +637,7 @@
                     curFromNodeChild = fromNextSibling;
                 }
 
-                // We have processed all of the "to nodes". If curFromNodeChild is
-                // non-null then we still have some from nodes left over that need
-                // to be removed
-                while (curFromNodeChild) {
-                    fromNextSibling = curFromNodeChild.nextSibling;
-                    if ((curFromNodeKey = getNodeKey(curFromNodeChild))) {
-                        // Since the node is keyed it might be matched up later so we defer
-                        // the actual removal to later
-                        addKeyedRemoval(curFromNodeKey);
-                    } else {
-                        // NOTE: we skip nested keyed nodes from being removed since there is
-                        //       still a chance they will be matched up later
-                        removeNode(curFromNodeChild, fromEl, true /* skip keyed nodes */);
-                    }
-                    curFromNodeChild = fromNextSibling;
-                }
+                cleanupFromEl(fromEl, curFromNodeChild, curFromNodeKey);
 
                 var specialElHandler = specialElHandlers[fromEl.nodeName];
                 if (specialElHandler) {
