63ad7b58e287edc6601fbb5a415b1242debb5aec
snewcomer <snewcomer24@gmail.com>
2019-04-05 15:24:30 -0700

    add test for default selected option

--- test/fixtures/autotest/select-element-default/index.js
+++ test/fixtures/autotest/select-element-default/index.js
@@ -1,6 +1,6 @@
 exports.verify = function (context, expect) {
     var rootNode = context.rootNode;
     var selectNode = rootNode.querySelector('select');
-    expect(selectNode.selectedIndex).to.equal(2);
-    expect(selectNode.children[2].selected).to.equal(true);
+    expect(selectNode.selectedIndex).to.equal(0);
+    expect(selectNode.firstElementChild.selected).to.equal(true);
 };
