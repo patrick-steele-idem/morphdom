ec033e79e9a10f1cb50d5382be06209710e10528
snewcomer <snewcomer24@gmail.com>
2019-08-04 13:23:26 -0700

    new dist

--- dist/morphdom-factory.js
+++ dist/morphdom-factory.js
@@ -4,7 +4,30 @@ var range; // Create a range object for efficently rendering strings to elements
 var NS_XHTML = 'http://www.w3.org/1999/xhtml';
 
 var doc = typeof document === 'undefined' ? undefined : document;
-var hasTemplateSupport = 'content' in document.createElement('template');
+var HAS_TEMPLATE_SUPPORT = !!doc && 'content' in doc.createElement('template');
+var HAS_RANGE_SUPPORT = !!doc && doc.createRange && 'createContextualFragment' in doc.createRange();
+
+function createFragmentFromTemplate(str) {
+    var template = doc.createElement('template');
+    template.innerHTML = str;
+    return template.content.childNodes[0];
+}
+
+function createFragmentFromRange(str) {
+    if (!range) {
+        range = doc.createRange();
+        range.selectNode(doc.body);
+    }
+
+    var fragment = range.createContextualFragment(str);
+    return fragment.childNodes[0];
+}
+
+function createFragmentFromWrap(str) {
+    var fragment = doc.createElement('body');
+    fragment.innerHTML = str;
+    return fragment.childNodes[0];
+}
 
 /**
  * This is about the same
@@ -15,26 +38,16 @@ var hasTemplateSupport = 'content' in document.createElement('template');
  * @param {String} str
  */
 function toElement(str) {
-    if (!range && doc.createRange) {
-        range = doc.createRange();
-        range.selectNode(doc.body);
-    }
-
-    var fragment;
-    if (hasTemplateSupport) {
+    if (HAS_TEMPLATE_SUPPORT) {
       // avoid restrictions on content for things like `<tr><th>Hi</th></tr>` which
       // createContextualFragment doesn't support
       // <template> support not available in IE
-      var template = document.createElement('template');
-      template.innerHTML = str.trim();
-      fragment = template.content;
-    } else if (range && range.createContextualFragment) {
-        fragment = range.createContextualFragment(str);
-    } else {
-        fragment = doc.createElement('body');
-        fragment.innerHTML = str;
+      return createFragmentFromTemplate(str);
+    } else if (HAS_RANGE_SUPPORT) {
+      return createFragmentFromRange(str);
     }
-    return fragment.childNodes[0];
+
+    return createFragmentFromWrap(str);
 }
 
 /**
