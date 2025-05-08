45696269fb8da77345dc96c8fdd1b938b5b1e5f7
Steffen Deusch <steffen@deusch.me>
2025-05-08 12:40:00 +0200

    fix typescript type definition

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,14 @@
 interface MorphDomOptions {
     getNodeKey?: (node: Node) => any;
-    onBeforeNodeAdded?: (node: Node) => Node;
-    onNodeAdded?: (node: Node) => Node;
+    onBeforeNodeAdded?: (node: Node) => false | Node;
+    onNodeAdded?: (node: Node) => void;
     onBeforeElUpdated?: (fromEl: HTMLElement, toEl: HTMLElement) => boolean;
     onElUpdated?: (el: HTMLElement) => void;
     onBeforeNodeDiscarded?: (node: Node) => boolean;
     onNodeDiscarded?: (node: Node) => void;
     onBeforeElChildrenUpdated?: (fromEl: HTMLElement, toEl: HTMLElement) => boolean;
+    skipFromChildren?: (fromEl: HTMLElement) => boolean;
+    addChild?: (parent: HTMLElement, child: HTMLElement) => void;
     childrenOnly?: boolean;
 }
 
@@ -14,6 +16,6 @@ declare function morphdom(
     fromNode: Node,
     toNode: Node | string,
     options?: MorphDomOptions,
-): void;
+): Node;
 
-export = morphdom;
+export default morphdom;
